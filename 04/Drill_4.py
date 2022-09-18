399d99f70947ffb5221ed3ae09c10ef6db530801
SiYeungLee@TIP209 <LeeSiYeoung.h4801y@naver.com>
2022-09-18 15:44:08 +0900

    Update Drill_4.py
    x-> y 로 변경, y -> x로 변경

--- 04/Drill_4.py
+++ 04/Drill_4.py
@@ -7,20 +7,20 @@ count = 4
 turtle.penup()
 
 turtle.pendown()
-while (x < 5):
-    y = 0
-    while(y < 5):
+while (y < 5):
+    x = 0
+    while(x < 5):
         count = 4
         while(count > 0):
             turtle.forward(100)
             turtle.left(90)
             count -=1
-        y+=1
+        x+=1
         turtle.forward(100)
-    x+=1
+    y+=1
 
     turtle.penup()
-    turtle.goto(0 , x * 100)
+    turtle.goto(0 , y * 100)
     turtle.pendown()
 
 turtle.exitonclick()
