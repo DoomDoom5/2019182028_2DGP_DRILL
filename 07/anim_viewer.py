fbebbebad950058f7bc4ec399f688964747df28b
SiYeungLee@TIP209 <LeeSiYeoung.h4801y@naver.com>
2022-09-28 15:03:32 +0900

    anim_viewer에 애니매이션 추가

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -1,5 +1,6 @@
 from pico2d import *
 open_canvas()
+
 grass = load_image('grass.png')
 character = load_image('Ice_Kirby_empty.png')
 
@@ -25,6 +26,10 @@ def Kirby_spin(x = 0,y = 0,frame = 0):
         frame = frame%8
         character.clip_draw(6 + frame * 40, 616 - 605, 40, 33, x, y)
 
+def Kirby_Rotate(x = 0,y = 0,frame = 0):
+        frame = frame%8
+        character.clip_draw(11 + frame * 37, 616 - 559, 37, 36, x, y)
+
 def Ice_Effect(x = 0,y = 0,frame = 0):
         frame = frame%4
         character.clip_draw(527 + frame * 72, 616 - 300, 72, 78, x, y)
@@ -39,6 +44,7 @@ while True:
         Kirby_dumbring(50,210, frame)
         Kirby_frying(50,280, frame)
         Kirby_spin(50,350, frame)
+        Kirby_Rotate(50,490, frame)
         Ice_Effect(50,420, frame)
 
         frame = (frame + 1) % 20
